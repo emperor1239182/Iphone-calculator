3d8cc13adc5f83d0d32ed14921c3d52710f22500
Xpression <emmybanks03@gmail.com>
2024-12-24 01:36:16 +0100

    convert

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let result = document.getElementById("result");
+let posneg = document.getElementsByClassName("unique");
 function show(number){
 	result.value += number;
 }
@@ -12,3 +13,52 @@ function clearResult() {
 	
 	result.value = "";
 }
+function cancel(){
+	result.value = result.value.slice(0, -1);
+}
+function change(){
+	if(result.value > -0){
+		result.value = "-".concat(result.value);
+	} else{
+		result.value = "+".concat(result.value);
+	}
+}
+
+function toKelvin(){
+	let result = document.getElementById("result").value;
+    let output = parseFloat(result)+ 273.15;
+    return "temp" + " " + "is" + ":" + output + "K";
+    }
+
+    function toCelcius(){
+		let result = document.getElementById("result").value;
+    let output = parseFloat(result) - 273.15;
+    return "temp" + " " + "is" + ":" + output + "C";
+    }
+
+	let convert = document.getElementById("conversion");
+	let metric = document.getElementById("metrics");
+	let execute = document.getElementById("convert");
+	let answer;
+	
+			execute.addEventListener("click", ()=>{
+				if(metric.value === "celcius"){
+					convert.innerHTML = "";
+				answer = toCelcius();
+				} else if(metric.value === "kelvin"){
+					convert.innerHTML = "";
+					answer = toKelvin();
+				}
+				 else{
+					answer = "enter a unit";
+				}
+				convert.innerHTML = answer;
+			}
+		);
+	function clear(){
+		if(metric.value === "none"){
+			convert.innerHTML = "";
+		}
+		
+	}
+	
