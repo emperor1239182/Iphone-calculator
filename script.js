8674bf3bc87cc3c77c54a45949cac931d8bb6b48
Xpression <emmybanks03@gmail.com>
2024-12-28 13:46:36 +0100

    weight

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let units = document.getElementById("metrics");
 let details = document.getElementById("details");
 let convert = document.getElementById("convert");
 
+let switchElement = document.getElementById("switch").value
+		let output = document.getElementById("output");
+		
+
 function show(number){
 	result.value += number;
 }
@@ -29,18 +33,19 @@ function change(){
 	}
 }
 
+//conversions display
 function display(){
-	if(units.value === "temperature" || units.value === "length" || units.value === "distance"){
+	if(units.value === "temperature" || units.value === "weight" || units.value === "distance"){
 	met.style.display = "block";
-	details.innerHTML = "Convert" + " " + units.value
-	}
+	details.innerHTML = "Convert" + " " + units.value;
+	} 
 	else{
 		met.style.display = "none"
 	}
 }
 
 
-
+//temperature conversion
   function toKelvin(temp){
     return temp + 273.15;
     }
@@ -49,25 +54,68 @@ function display(){
     return temp - 273.15;
     }
 
+	//mass conversion
+	function toKg(weight){
+		return weight / 1000;
+	}
+
+	function toGram(weight){
+		return weight * 1000;
+	}
 	
+	//trigger the conversion functions
 	convert.addEventListener("click", ()=>{
-		let switchElement = document.getElementById("switch").value
+		if(units.value === "temperature"){
+			return showTemp();
+		} else if(units.value === "weight"){
+			return showWeigth();
+		}	
+		})
+
+		//show temperature conversion result 
+		function showTemp(){
+		let switchElement = document.getElementById("switch").value;
 		let output = document.getElementById("output");
-		let input = document.getElementById("input").value
+		let input = document.getElementById("input").value;
+
 		if(input === ""){
 			return output.innerHTML = "Enter a valid input";
 		}
-			let temperature = parseFloat(input);
 
+		let realValue = parseFloat(input);
+			let temperature = realValue;
+
+		if(switchElement === "kelvin"){
+			output.innerHTML = "";
+			let solution = toKelvin(temperature);
+			output.innerHTML  = `Temperature is ${solution} K`;
+		} else if(switchElement === "celcius"){
+			output.innerHTML = "";
+			let solution = toCelcius(temperature);
+			output.innerHTML  = `Temperature is ${solution} C`;
+		}
+		}
+		//show weight conversion result
+		function showWeigth(){
+			let switchElement = document.getElementById("switch").value;
+			let output = document.getElementById("output");
+			let input = document.getElementById("input").value;
+
+			if(input === ""){
+				return output.innerHTML = "Enter a valid input";
+			}
+	
+			let realValue = parseFloat(input);
+				let weight = realValue;
+	
 			if(switchElement === "kelvin"){
 				output.innerHTML = "";
-				let solution = toKelvin(temperature);
-				output.innerHTML  = `Temperature is ${solution} K`;
+				let solution = toKg(weight);
+				output.innerHTML  = `Weight is ${solution} Kg`;
 			} else if(switchElement === "celcius"){
 				output.innerHTML = "";
-				let solution = toCelcius(temperature);
-				output.innerHTML  = `Temperature is ${solution} C`;
+				let solution = toGram(weight);
+				output.innerHTML  = `Weight is ${solution} G`;
 			}
-			
-		})
+			}
 
